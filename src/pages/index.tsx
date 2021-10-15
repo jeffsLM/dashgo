657195cf0d366ad062d0b46fd7e844bcb3862ede
jeffsLM <jeffes.dev@gmail.com>
2021-10-14 21:27:45 -0300

    add singIn page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,31 @@
-import Head from 'next/head'
+import { Flex, Button, Stack } from '@chakra-ui/react'
 
+import {Input} from '../components/Form/Input'
 
-export default function Home() {
+export default function SingIn() {
   return (
-    <h1>
-      dash.go
-    </h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+
+        <Stack spacing="4">
+          <Input type="email" name="email" label="Email" />
+          <Input type="password" name="password" label="Password" />
+        </Stack>
+
+        <Button type="submit" size="lg" mt="6" colorScheme="pink">Entrar </Button>
+      </Flex>
+    </Flex>
   )
 }
