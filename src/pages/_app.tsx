8abe193782c1fcafbb02c85b75c70a75b7292726
jeffsLM <jeffes.dev@gmail.com>
2021-11-02 09:57:06 -0300

    add config query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,28 @@
 import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClientProvider,QueryClient } from 'react-query'
 
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
 import { theme } from '../styles/theme';
 import { makeServer } from '../services/mirage';
 
 
-if(process.env.NODE_ENV === 'development'){
+if (process.env.NODE_ENV === 'development') {
   makeServer()
 }
 
+const queryClient = new QueryClient()
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
-    </ChakraProvider>)
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
