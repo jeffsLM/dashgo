473e6aed221177ba5ee315a27bbe4f5e4c213344
jeffsLM <jeffes.dev@gmail.com>
2021-11-02 08:37:40 -0300

    add factory e seeds

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,12 @@ import { ChakraProvider } from '@chakra-ui/react';
 
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
 import { theme } from '../styles/theme';
+import { makeServer } from '../services/mirage';
+
+
+if(process.env.NODE_ENV === 'development'){
+  makeServer()
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
